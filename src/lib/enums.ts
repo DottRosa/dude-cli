f5425875fd9cbc2e1be2dd88a0c30326ce133b28
Marco Rosa <marco1rosa@gmail.com>
2024-05-05 16:14:14 +0200

    Create new enums and types for the validation of the config file

---
+++ src/lib/enums.ts
@@ -0,0 +1,22 @@
+export enum CONFIG_ACTION_FIELD_TYPES_ENUM {
+  STRING = 'string',
+  LIST = 'list',
+}
+
+export enum CONFIG_VALIDATION_LOG_LEVEL_ENUM {
+  INFO = 'info',
+  WARNING = 'warning',
+  ERROR = 'error',
+}
+
+export enum CONFIG_VALIDATION_LOG_MESSAGE_TYPES_ENUM {
+  MISSING_FIELD = 'missing_field',
+  WRONG_VALUE = 'wrong_value',
+  EMPTY_LIST = 'empty_list',
+}
+
+export enum CONFIG_VALIDATION_LOG_MESSAGE_FIELD_TYPES_ENUM {
+  STRING = 'string',
+  BOOLEAN = 'boolean',
+  LIST = 'list',
+}
